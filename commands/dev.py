381a1c673eafd7f8214c964071e2023ca6e9c5d9
Shayklos <shaymin1010@gmail.com>
2023-09-13 19:41:21 +0200

    Added dev commands for disabling commands and changing status

--- commands/dev.py
+++ commands/dev.py
@@ -12,6 +12,7 @@ class DevCommands(commands.Cog):
         self.bot = bot 
 
 
+
     @commands.command()
     async def ping(self, ctx: commands.Context):
         if ctx.author.name not in admins:
@@ -19,6 +20,7 @@ class DevCommands(commands.Cog):
         
         print("/ping was called by", ctx.author.name)
         await ctx.send(f"Average websocket latency: {round(self.bot.latency * 1000, 2)}ms")
+
 
 
     @commands.command()
@@ -34,13 +36,90 @@ class DevCommands(commands.Cog):
 
         if command == 'all':
             for extension in ["".join(('commands.', extension[:-3])) for extension in os.listdir('commands') if extension[-3:] == '.py']:
-                await self.bot.reload_extension(extension)
+                try:
+                    await self.bot.reload_extension(extension)
+                except:
+                    await self.bot.load_extension(extension)
 
         else:
-            await self.bot.reload_extension('commands.' + command)
+            try:
+                await self.bot.reload_extension('commands.' + command)
+            except:
+                await self.bot.load_extension('commands.' + command)
 
 
         await ctx.message.add_reaction("👍")
+
+
+
+    @commands.command()
+    async def disable(self, ctx: commands.Context, command: str): 
+        """
+        Disables a command/ all commands
+        """
+        
+        if ctx.author.name not in admins:
+            return
+        
+        print("/disable was called by", ctx.author.name)
+
+        if command == 'all':
+            for extension in ["".join(('commands.', extension[:-3])) for extension in os.listdir('commands') if extension[-3:] == '.py']:
+                await self.bot.unload_extension(extension)
+
+        else:
+            await self.bot.unload_extension('commands.' + command)
+
+
+        await ctx.message.add_reaction("👍")
+
+
+
+    @commands.command()
+    async def change_status(self, ctx: commands.Context, status: str): 
+        """
+        Changes bot status (online, idle, dnd, offline)
+        """
+        
+        if ctx.author.name not in admins:
+            return
+        
+        print("/change_status was called by", ctx.author.name)
+
+        match status:
+            case "online":
+                await self.bot.change_presence(status=discord.Status.online)
+            case "offline":
+                await self.bot.change_presence(status=discord.Status.invisible)
+            case "idle":
+                await self.bot.change_presence(status=discord.Status.idle)
+            case "dnd":
+                await self.bot.change_presence(status=discord.Status.dnd)
+            case other:
+                await ctx.message.add_reaction("👎")
+                return
+            
+        await ctx.message.add_reaction("👍")
+
+
+
+    @commands.command()
+    async def commands(self, ctx: commands.Context): 
+        """
+        Displays list of dev commands
+        """
+        
+        if ctx.author.name not in admins:
+            return
+        
+        print("/commands was called by", ctx.author.name)
+
+        await ctx.reply("""ping
+reload [command | all]
+disable [command |all]
+change_status [online | offline | idle | dnd]
+commands
+""", ephemeral=False)
 
 
 async def setup(bot: commands.Bot):
