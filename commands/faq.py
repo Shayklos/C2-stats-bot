4f86591ecf5d88d3e6ee135cf0bda3084a40814d
Shayklos <shaymin1010@gmail.com>
2024-02-13 18:12:29 +0100

    Update /faq

--- commands/faq.py
+++ commands/faq.py
@@ -10,7 +10,9 @@ aliases = {
     'webmaster' : ['web', 'website', 'update', 'updates'],
     'mac' : ['macos'],
     'patch' : ['patches', 'mod'],
-    'help' : []
+    'help' : [],
+    'simon' : ['c2developer', 'c2devs', 'c2dev', 'dev', 'devs', 'developer'],
+    'c1soundtrack' : ['c1ost', 'c1music', 'c1songs'],
 }
 
 class FAQ_Commands(commands.Cog):
@@ -28,17 +30,22 @@ class FAQ_Commands(commands.Cog):
 
     @faq.command(aliases = aliases['help'])
     async def help(self, ctx: commands.Context):
-        await ctx.reply(dedent("""
-            /faq web
-            /faq mac
-            /faq patch
-            /faq help"""), ephemeral = True)
+        s = ""
+        for command in aliases.keys():
+            s += f"/faq {command}{' '*(20 - len(command))}{aliases.get(command)}\n"
+        await ctx.reply(dedent(s), ephemeral = True)
         
     @faq.command(aliases = aliases['webmaster'])
     async def webmaster(self, ctx: commands.Context):
         await ctx.reply(dedent("""
             Simon, Cultris II developer, has stated he will update the game only after the game has a webmaster.
             Anyone can apply to be one, although this would be a long term job with no pay. Contact Simon via email: `de@iru.ch`"""))
+            
+    @faq.command(aliases = aliases['simon'])
+    async def simon(self, ctx: commands.Context):
+        await ctx.reply(dedent("""
+            Cultris II only developer is Simon Felix. The only way to get in touch with Simon is via email: `de@iru.ch`"""))       
+        
         
     @faq.command(aliases = aliases['mac'])
     async def mac(self, ctx: commands.Context):
@@ -54,7 +61,15 @@ class FAQ_Commands(commands.Cog):
             The Cultris II patch made by Def, significantly improves the game's performance.
             [You can download it here](https://github.com/zDEFz/c2-patch). There's a video in the description with indications in how to install the patch (it's easy!). This patch also allows disabling some ingame sounds, for additional performance gain."""))
 
-
+    @faq.command(aliases = aliases['c1soundtrack'])
+    async def c1soundtrack(self, ctx: commands.Context):
+        await ctx.reply(dedent("""
+            [Here's a list of the songs in Cultris I](https://docs.google.com/spreadsheets/d/1MPaLgEnlx9bNBmmwzxY0UeiotZ7LZ8VXz64qYdMU24I)."""))
+        
+    
+    ##FAQ_END
+    
+    
 async def setup(bot: commands.Bot):
     await bot.add_cog(FAQ_Commands(bot))
     print(f"Loaded /faq command.")
