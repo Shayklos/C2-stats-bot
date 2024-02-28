cb141f64d0978fa25a570dd7f1ae6c24a680d4f5
Shayklos <shaymin1010@gmail.com>
2024-02-28 13:54:55 +0100

    Insert faq questions through external file

--- commands/faq.py
+++ commands/faq.py
@@ -12,49 +12,7 @@ class FAQ_Commands(commands.Cog):
     def __init__(self, bot: commands.Bot):
         super().__init__()
         self.bot = bot 
-       
-    # @commands.group()
-    # async def faq(self, ctx: commands.Context):
-    #     if ctx.invoked_subcommand is None:
-    #         command = self.bot.get_command('faq help')
-    #         ctx.command = command
-    #         ctx.invoked_subcommand = command
-    #         await self.bot.invoke(ctx)
-
-    # @faq.command(aliases = aliases['webmaster'])
-    # async def webmaster(self, ctx: commands.Context):
-    #     await ctx.reply(dedent("""
-    #         Simon, Cultris II developer, has stated he will update the game only after the game has a webmaster.
-    #         Anyone can apply to be one, although this would be a long term job with no pay. Contact Simon via email: `de@iru.ch`"""))
-            
-    # @faq.command(aliases = aliases['simon'])
-    # async def simon(self, ctx: commands.Context):
-    #     await ctx.reply(dedent("""
-    #         Cultris II only developer is Simon Felix. The only way to get in touch with Simon is via email: `de@iru.ch`"""))       
-        
-        
-    # @faq.command(aliases = aliases['mac'])
-    # async def mac(self, ctx: commands.Context):
-    #     await ctx.reply(dedent("""
-    #         There's no official support for macOS.
-    #         However, some users have reported they were able to play using external software, check:
-    #         https://discord.com/channels/202843125633384448/516686648201707548/1182624707716055040
-    #         https://discord.com/channels/202843125633384448/516689168223567872/1042891393019953172"""))
-
-    # @faq.command(aliases = aliases['patch'])
-    # async def patch(self, ctx: commands.Context):
-    #     await ctx.reply(dedent("""
-    #         The Cultris II patch made by Def, significantly improves the game's performance.
-    #         [You can download it here](https://github.com/zDEFz/c2-patch). There's a video in the description with indications in how to install the patch (it's easy!). This patch also allows disabling some ingame sounds, for additional performance gain."""))
-
-    # @faq.command(aliases = aliases['c1soundtrack'])
-    # async def c1soundtrack(self, ctx: commands.Context):
-    #     await ctx.reply(dedent("""
-    #         [Here's a list of the songs in Cultris I](https://docs.google.com/spreadsheets/d/1MPaLgEnlx9bNBmmwzxY0UeiotZ7LZ8VXz64qYdMU24I)."""))
-        
-    # @commands.group()
-    # async def faq(self, ctx: commands.Context, question: str):
-        
+               
     @staticmethod
     def available_faqs(aliases_dict: dict, long = True) -> str:
         s = ""
