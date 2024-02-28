cba6a21d0cbda60e93b6afa363fa22580438ce15
Shayklos <shaymin1010@gmail.com>
2024-02-28 13:38:46 +0100

    Initial FAQ alias logic

--- commands/faq.py
+++ commands/faq.py
@@ -60,7 +60,7 @@ class FAQ_Commands(commands.Cog):
         s = ""
         for name, aliases in aliases_dict.items():
             line = f"/faq {name}"
-            if long : line += " | " + " | ".join(alias for alias in aliases)
+            if long and aliases : line += " | " + " | ".join(alias for alias in aliases)
             s += line + '\n'
 
         # Avoid reaching Discord message limit of 2000 characters. Now, you should think about separating this into different messages. Reflect on what you've done and what you've accomplished. Consider the consequences of your acts.
@@ -71,8 +71,6 @@ class FAQ_Commands(commands.Cog):
     async def faq(self, ctx: commands.Context, question: str):
         with open("files/faq.json") as f: faq = json.load(f)
         
-        print(ctx.invoked_subcommand)
-        print(question)
         # Check if the question is directly in faqs
         if answer := faq['faqs'].get(question):
             await ctx.reply(content = answer)
@@ -88,18 +86,61 @@ class FAQ_Commands(commands.Cog):
             else:
                 await ctx.reply(content = FAQ_Commands.available_faqs(faq['aliases']))
 
-    @faq.command()
-    async def add(self, ctx: commands.Context, question: str, *, answer: str):
-        print("question:", question, "answer", answer)
+    @faq.group()
+    async def add(self, ctx: commands.Context):
+        if ctx.invoked_subcommand is None:
+            await ctx.reply(content="/faq add question [question] [answer]\n /faq add alias [question] [new_alias] [new_alias] ... [new_alias]")
+
+    @add.command(name="question")
+    async def add_question(self, ctx: commands.Context, question: str, *, answer: str):
         with open("files/faq.json") as f: faq = json.load(f)
         faq['faqs'][question] = answer
+        faq['aliases'][question] = []
         with open("files/faq.json", 'w') as f: json.dump(faq, f)
 
-    @faq.command()
-    async def delete(self, ctx: commands.Context, question):
+    @add.command(name="alias", aliases = ["aliases"])
+    async def add_alias(self, ctx: commands.Context, question: str, *, new_aliases: str):
         with open("files/faq.json") as f: faq = json.load(f)
-        try: faq['faqs'].pop(question)
-        except KeyError: print("No question with that name.")
+        if faq['aliases'].get(question) is not None: 
+            for alias in new_aliases.split(): faq['aliases'][question].append(alias.lower())
+        else:
+            for name, aliases in faq['aliases'].items():
+                if question in aliases:
+                    for alias in new_aliases.split(): 
+                        faq['aliases'][name].append(alias.lower())
+                    break
+            else:
+                await ctx.reply(content = "No question with that name.")
+        with open("files/faq.json", 'w') as f: json.dump(faq, f)
+
+    @faq.group(aliases = ["remove"])
+    async def delete(self, ctx: commands.Context):
+        if ctx.invoked_subcommand is None:
+            await ctx.reply(content="/faq delete question [question] [answer]\n /faq delete alias [question] [alias_to_remove] [alias_to_remove] ... [alias_to_remove]")
+        
+    @delete.command(name="question")
+    async def delete_question(self, ctx: commands.Context, question: str):
+        with open("files/faq.json") as f: faq = json.load(f)
+        try: 
+            faq['faqs'].pop(question)
+            faq['aliases'].pop(question)
+            
+        except KeyError: await ctx.reply(content="No question with that name.")
+        with open("files/faq.json", 'w') as f: json.dump(faq, f)
+
+    @delete.command(name="alias", aliases = ["aliases"])
+    async def delete_alias(self, ctx: commands.Context, question: str, *, alias_to_remove: str):
+        with open("files/faq.json") as f: faq = json.load(f)
+        if faq['aliases'].get(question) is not None: 
+            for alias in alias_to_remove.split(): faq['aliases'][question].remove(alias)
+        else:
+            for name, aliases in faq['aliases'].items():
+                if question in aliases:
+                    for alias in alias_to_remove.split(): 
+                        faq['aliases'][name].remove(alias)
+                    break
+            else:
+                await ctx.reply(content = "No question with that name.")
         with open("files/faq.json", 'w') as f: json.dump(faq, f)
 
 
